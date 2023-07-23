c35fa432595ff6c50274fe5631d074fa15c7e734
joyogbu <obyjoy@yahoo.com>
2023-07-23 06:10:28 -0700

    task 0

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env  python3
+'''simple helper function that rerurns a tuple'''
+
+
+from typing import Tuple
+
+
+def index_range(page: int, page_size:int) -> Tuple[int, int]:
+    start_index = (page_size / page) * (page - 1)
+    last_index = page_size * page
+    #for i in range(page, page_size + 1):
+    #print 
+    return (int(start_index), last_index)
