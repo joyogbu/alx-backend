b3a5c969b410769134568926acc725f196313502
joyogbu <obyjoy@yahoo.com>
2023-07-23 16:31:49 -0700

    task 2

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -56,7 +56,7 @@ class Server:
             return []
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> \
-            Dict[str, Union[int, str, List[list]]]:
+            Dict[str, Union[int, str, List[List]]]:
         '''defining the function'''
         my_dict = {}
         res = self.dataset()
