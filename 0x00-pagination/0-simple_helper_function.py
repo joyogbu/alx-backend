c1d1b5471655c53a1b25a7a9fccbdc359f9fbb40
joyogbu <obyjoy@yahoo.com>
2023-07-23 06:12:50 -0700

    task 0

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -5,9 +5,10 @@
 from typing import Tuple
 
 
-def index_range(page: int, page_size:int) -> Tuple[int, int]:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    '''defining the function'''
     start_index = (page_size / page) * (page - 1)
     last_index = page_size * page
-    #for i in range(page, page_size + 1):
-    #print 
+    # for i in range(page, page_size + 1):
+    # print
     return (int(start_index), last_index)
