784372aa11cb299ef1d2576cdcecfdd16fe3216d
joyogbu <obyjoy@yahoo.com>
2023-07-26 08:38:56 -0700

    edited 0

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -3,36 +3,6 @@
 
 
 from base_caching import BaseCaching
-'''class BaseCaching():
-    """ BaseCaching defines:
-      - constants of your caching system
-      - where your data are stored (in a dictionary)
-    """
-    MAX_ITEMS = 4
-
-    def __init__(self):
-        """ Initiliaze
-        """
-        self.cache_data = {}
-
-    def print_cache(self):
-        """ Print the cache
-        """
-        print("Current cache:")
-        for key in sorted(self.cache_data.keys()):
-            print("{}: {}".format(key, self.cache_data.get(key)))
-
-    def put(self, key, item):
-        """ Add an item in the cache
-        """
-        raise NotImplementedError("put must be implemented in\
-                your cache class")
-
-    def get(self, key):
-        """ Get an item by key
-        """
-        raise NotImplementedError("get must be implemented in\
-                your cache class")'''
 
 
 class BasicCache(BaseCaching):
@@ -47,11 +17,13 @@ class BasicCache(BaseCaching):
         '''defining the function'''
         if key is None or item is None:
             pass
-        self.my_dict[key] = item
+        else:
+            self.my_dict[key] = item
 
     def get(self, key):
         '''defining the function'''
-        # if key or key is not None:
+        if key is None:
+            return None
         try:
             return self.my_dict[key]
         except KeyError:
