1fea70c86f3a6e558cce0495ac7a5e2c36bb1146
joyogbu <obyjoy@yahoo.com>
2023-08-01 11:32:23 -0700

    task 0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -7,11 +7,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def index():
     '''defining the index page'''
     return render_template('index.html')
 
 
+'''running the flask app'''
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
