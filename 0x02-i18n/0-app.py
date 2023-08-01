d0c01d000ef58da092ea7899f4a192399826a79a
joyogbu <obyjoy@yahoo.com>
2023-08-01 14:34:30 -0700

    task 1

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -18,6 +18,7 @@ def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 """
 
+
 @app.route('/', strict_slashes=False)
 def index():
     '''defining the index page'''
