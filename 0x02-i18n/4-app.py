9241fa98176527e8a4995ecee03d6108a47ce76f
joyogbu <obyjoy@yahoo.com>
2023-08-02 22:32:09 +0000

    task 4

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+'''module for get locale'''
+
+
+from flask import Flask, render_template
+from flask_babel import Babel
+from flask import request
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    '''defining the function'''
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    '''defining the function'''
+    return render_template('2-index.html')
+
+
+'''runnng the app'''
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
