21395cbb0e20dda9fc28791be7b7e5cce1eb301b
joyogbu <obyjoy@yahoo.com>
2023-08-03 04:37:19 +0000

    task 4

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -14,7 +14,8 @@ babel = Babel(app)
 @babel.localeselector
 def get_locale() -> str:
     '''defining the function'''
-    if request.args.get(locale):
+    loc = request.args.get(locale)
+    if loc is not None:
         return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
