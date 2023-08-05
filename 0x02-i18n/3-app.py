80161b50d232076116ab6b77fbf8ab703397e42e
joyogbu <obyjoy@yahoo.com>
2023-08-05 16:09:47 +0000

    task 3

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -2,12 +2,28 @@
 '''module for parameterize template'''
 
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_babel import Babel, gettext as _
 
 
 app = Flask(__name__)
 babel = Babel(app)
+
+
+class Config(object):
+    '''defining class for babel configurations'''
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    '''defining the function'''
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/', strict_slashes=False)
