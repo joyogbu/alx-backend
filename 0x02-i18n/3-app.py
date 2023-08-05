b3c3b6b0afa7ffa76480ca1fe11f15399f7cb1a3
joyogbu <obyjoy@yahoo.com>
2023-08-05 15:40:05 +0000

    taskk 3

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -13,10 +13,9 @@ babel = Babel(app)
 @app.route('/', strict_slashes=False)
 def index():
     '''defining the gettext function'''
-    home_title = _('Welcome to Holberton')
-    home_header = _('Hello world!')
-    return render_template('3-index.html', home_title=home_title,
-                           home_header=home_header)
+    # home_title = _('Welcome to Holberton')
+    # home_header = _('Hello world!')
+    return render_template('3-index.html')
 
 
 if __name__ == "__main__":
